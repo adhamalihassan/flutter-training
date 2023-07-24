44f79bd7783338fcdf5923772dcd2ff6b1e24742
adhamalihassan <139714020+adhamalihassan@users.noreply.github.com>
2023-07-24 13:48:57 +0300

    fix const constructor error

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -13,12 +14,7 @@ class GradientContainer extends StatelessWidget {
         ]),
       ),
       child: const Center(
-        child: Text(
-          'hello world',
-          style: TextStyle(
-            fontSize: 20.0,
-          ),
-        ),
+        child: StyledText('hello world!'),
       ),
     );
   }
