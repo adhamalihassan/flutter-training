fb06e4ac41108f53a0bf713996d25b6124582c0c
adhamalihassan <139714020+adhamalihassan@users.noreply.github.com>
2023-07-26 00:27:09 +0300

    challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: GradientContainer.purple(),
+        backgroundColor: Colors.purple,
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/images/quiz-logo.png'),
+              const SizedBox(
+                height: 30.0,
+              ),
+              const Text(
+                'Learn Flutter the fun way!',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+                  backgroundColor: Colors.purple,
+                ),
+                child: const Text(
+                  'start quiz',
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
