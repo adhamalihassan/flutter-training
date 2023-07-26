fb218d2b265eb4a38b91f56a33681381f048add0
adhamalihassan <139714020+adhamalihassan@users.noreply.github.com>
2023-07-26 12:47:44 +0300

    add gradient of colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/start_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,40 +6,18 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.purple,
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/images/quiz-logo.png',
-                width: 300.0,
-                color: const Color.fromARGB(125, 255, 255, 255),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              const Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              OutlinedButton.icon(
-                onPressed: () {},
-                style: OutlinedButton.styleFrom(
-                  foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-                ),
-                label: const Text(
-                  'start quiz',
-                ),
-                icon: const Icon(Icons.arrow_right_alt),
-              ),
-            ],
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
+          child: const StartScreen(),
         ),
       ),
     ),
---
+++ lib/start_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300.0,
+            color: const Color.fromARGB(125, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 30.0,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+            ),
+            label: const Text(
+              'start quiz',
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+          ),
+        ],
+      ),
+    );
+  }
+}
