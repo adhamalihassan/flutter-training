57a55d6f77075c496894c6a497bfa5fabf252fac
adhamalihassan <139714020+adhamalihassan@users.noreply.github.com>
2023-07-26 10:57:02 +0300

    add opacity to image

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,11 @@ void main() {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Image.asset('assets/images/quiz-logo.png'),
+              Image.asset(
+                'assets/images/quiz-logo.png',
+                width: 300.0,
+                color: const Color.fromARGB(125, 255, 255, 255),
+              ),
               const SizedBox(
                 height: 30.0,
               ),
